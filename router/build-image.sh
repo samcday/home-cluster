d48c085bf6fad1c39c6dc7730d96183f9191caf2
Sam Day <me@samcday.com>
2023-09-01 18:16:45 +0200

    router: remove PXE boot + extstorage + cruft, simplify tang

--- router/build-image.sh
+++ router/build-image.sh
@@ -11,14 +11,13 @@ fi
 export WIFI_PASSWORD=${WIFI_PASSWORD:-$(bw get item "b612257d-22e8-4350-ad6a-afbf01069457" | jq -r .notes)}
 export TAILNET_AUTH_KEY=${TAILNET_AUTH_KEY:-$(bw get item "6e22f9a5-38aa-4703-8dfd-afc200fcb3ee" | jq -r .notes)}
 export ROOT_PW=${ROOT_PW:-$(bw get item "691cb088-5130-476c-ab7b-adb900fcdb8d" | jq -r .login.password)}
-export BOOT_TOKEN=$(sops -d ../boot-token)
 export INJECT_ENV='$WIFI_PASSWORD $TAILNET_AUTH_KEY $ROOT_PW $BOOT_TOKEN'
 
 # Misc bits of config
 export IPADDR="10.0.1.1"
 export IPADDR_RESTRICTED="10.0.2.1"
 export HOSTNAME="home-cluster-router"
-export SSID="samnet"
+export SSID="sam-home-cluster"
 export INJECT_ENV="$INJECT_ENV $(echo '$HOSTNAME $SSID $IPADDR $IPADDR_RESTRICTED')"
 
 if [[ ! -f _build/.setup ]]; then
@@ -44,9 +43,7 @@ export BIN_DIR="."
 export FILES="files"
 export PACKAGES=$(echo $(cat packages))
 export PROFILE=avm_fritzbox-4040
-# using openssh-server instead
-# tailscale + xinetd are started once extstorage is mounted in rc.local
-export DISABLED_SERVICES="dropbear tailscale xinetd"
+export DISABLED_SERVICES="dropbear" # using openssh-server instead
 
 (
   cd _build/
