5650398029e360162d829333c86e671c04671dcf
Sam Day <me@samcday.com>
2023-09-13 10:50:15 +0200

    terraform: home-cluster Flux webhook

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "plukevdh/dmsnitch"
       version = "0.1.5"
     }
+    github = {
+      source  = "integrations/github"
+      version = "5.32.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.22.0"
@@ -14,6 +18,7 @@ terraform {
 }
 
 provider "dmsnitch" {}
+provider "github" {}
 provider "kubernetes" {}
 
 resource "dmsnitch_snitch" "home-cluster" {
@@ -32,4 +37,33 @@ resource "kubernetes_secret" "dms-url" {
   data = {
     "url" = dmsnitch_snitch.home-cluster.url
   }
+}
+
+data "kubernetes_secret" "webhook-token" {
+  metadata {
+    name      = "webhook-token"
+    namespace = "flux-system"
+  }
+}
+
+data "kubernetes_resource" "receiver" {
+  api_version = "notification.toolkit.fluxcd.io/v1"
+  kind        = "Receiver"
+
+  metadata {
+    name      = "home-cluster"
+    namespace = "flux-system"
+  }
+}
+
+resource "github_repository_webhook" "push" {
+  active = true
+  configuration {
+    content_type = "form"
+    insecure_ssl = false
+    secret       = data.kubernetes_secret.webhook-token.data.token
+    url          = "https://home-flux.samcday.com${data.kubernetes_resource.receiver.object.status.webhookPath}"
+  }
+  events     = ["push"]
+  repository = "home-cluster"
 }
