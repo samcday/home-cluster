a1e5ad7420dc148692ac999a69eb812e0a7309d8
Sam Day <me@samcday.com>
2023-10-09 14:50:42 +0200

    devices/librem13: initial (incomplete) stab at Arch mkosi image
    mostly imported from hominions repo, updated for new mkosi version

---
+++ devices/librem13/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -uexo pipefail
+
+rm -rf .mkosi.extra.enc.dec
+mkdir .mkosi.extra.enc.dec
+for f in $(find mkosi.extra.enc -type f); do
+    f="${f#mkosi.extra.enc\/}"
+    mkdir -p .mkosi.extra.enc.dec/$(dirname $f)
+    sops -d mkosi.extra.enc/$f > .mkosi.extra.enc.dec/$f
+
+done
---
+++ devices/librem13/mkosi.extra/etc/profile.d/etcdctl.sh
@@ -0,0 +1,3 @@
+export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/peer.crt
+export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
+export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/peer.key
