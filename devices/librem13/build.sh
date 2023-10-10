8de21d9d30608243e5912daf53c4c497982fa974
Sam Day <me@samcday.com>
2023-10-10 10:33:42 +0200

    devices/librem13: mostly done now

--- devices/librem13/build.sh
+++ devices/librem13/build.sh
@@ -7,5 +7,6 @@ for f in $(find mkosi.extra.enc -type f); do
     f="${f#mkosi.extra.enc\/}"
     mkdir -p .mkosi.extra.enc.dec/$(dirname $f)
     sops -d mkosi.extra.enc/$f > .mkosi.extra.enc.dec/$f
-
 done
+
+mkosi -f
--- devices/librem13/mkosi.extra/etc/profile.d/etcdctl.sh
+++
@@ -1,3 +0,0 @@
-export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/peer.crt
-export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
-export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/peer.key
