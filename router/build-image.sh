2adff1f060077b3079c7f524d9a9d01186218b0a
Sam Day <me@samcday.com>
2024-05-10 19:26:42 +0200

    router: switch secret handling to sops

--- router/build-image.sh
+++ router/build-image.sh
@@ -4,22 +4,12 @@ set -uexo pipefail
 
 openwrt_version="23.05.3"
 
-# Secrets can be set explicitly, otherwise they're taken from my Bitwarden vault by default.
-if [[ -z "${BW_SESSION:-}" ]]; then
-  export BW_SESSION=$(bw unlock --raw)
-fi
-
-# Secrets
-export WIFI_PASSWORD=${WIFI_PASSWORD:-$(bw get item "b612257d-22e8-4350-ad6a-afbf01069457" | jq -r .notes)}
-export ROOT_PW=${ROOT_PW:-$(bw get item "691cb088-5130-476c-ab7b-adb900fcdb8d" | jq -r .login.password)}
-export INJECT_ENV='$WIFI_PASSWORD $ROOT_PW $BOOT_TOKEN'
-
 # Misc bits of config
 export IPADDR="10.0.1.1"
 export IPADDR_RESTRICTED="10.0.2.1"
 export HOSTNAME="home-cluster-router"
 export SSID="sam-home-cluster"
-export INJECT_ENV="$INJECT_ENV $(echo '$HOSTNAME $SSID $IPADDR $IPADDR_RESTRICTED')"
+export INJECT_ENV='$HOSTNAME $SSID $IPADDR $IPADDR_RESTRICTED'
 
 if [[ ! -f _build/.setup ]]; then
   mkdir -p _build/
