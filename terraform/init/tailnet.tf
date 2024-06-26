2207c5edc91dbfba1e24499c77c163b0b4bd2655
Sam Day <me@samcday.com>
2024-06-26 15:46:28 +0200

    tailscale: remove old k8s vip

--- terraform/init/tailnet.tf
+++ terraform/init/tailnet.tf
@@ -9,9 +9,6 @@ resource "tailscale_acl" "acl" {
         # home-cluster Service CIDRs
         "172.31.0.0/16" : ["tag:home-cluster"],
         "fd7f:bc81:7c5c::/112" : ["tag:home-cluster"],
-
-        # home-cluster control-plane endpoint VIP
-        "172.29.0.1/32" : ["tag:home-cluster"],
       },
     },
 
@@ -26,10 +23,7 @@ resource "tailscale_acl" "acl" {
     ],
 
     "tests" : [
-      {
-        "src" : "tag:home-cluster",
-        "accept" : ["172.29.0.1:6444"],
-      }
+
     ],
   })
 }
