e05c787c38400560bfab3344fae06f72ca5b38ff
Sam Day <me@samcday.com>
2024-07-02 11:03:07 +0200

    pixiecore: make boot API authoritative
    if we can't figure out how to boot the machine, then default to sending
    an iPXE exit command so that the machine continues local boot flow.

--- cluster/kube-system/pixiecore-api/boot-request.sh
+++ cluster/kube-system/pixiecore-api/boot-request.sh
@@ -8,25 +8,29 @@ kubectl="chroot /host kubectl"
 mac_addr=$(basename "$REQUEST_URI" | tr '[:upper:]' '[:lower:]' | tr ':' '-')
 node=$($kubectl get node -o name -l "samcday.com/mac=$mac_addr" | head -n1)
 
-if [[ -z "$node" ]]; then
-  echo "ignition request for unknown mac $mac_addr" >&2
-  echo "Status: 404"
-  echo
+echo "content-type: application/json"
+echo
+
+function booterr() {
+  echo $1 >&2
+  cat <<HERE
+{
+  "ipxe-script": "#!ipxe\nexit"
+}
+HERE
   exit
+}
+
+if [[ -z "$node" ]]; then
+  booterr "ignition request for unknown mac $mac_addr"
 fi
 
 if ! $kubectl get "$node" -o jsonpath='{.metadata.annotations}' | jq -e '. | keys | any(. == "samcday.com/boot")' >/dev/null 2>&1; then
-  echo "ignition request for $node which is missing boot annotation" >&2
-  echo "Status: 404"
-  echo
-  exit
+  booterr "ignition request for $node which is missing boot annotation"
 fi
 
 if ! $kubectl get "$node" -o jsonpath='{.metadata.annotations}' | jq -e '. | keys | any(. == "samcday.com/boot-profiles")' >/dev/null 2>&1; then
-  echo "ignition request for $node which is missing boot-profiles annotation" >&2
-  echo "Status: 404"
-  echo
-  exit
+  booterr "ignition request for $node which is missing boot-profiles annotation"
 fi
 
 resp=$(curl --fail https://builds.coreos.fedoraproject.org/streams/stable.json | jq .architectures.x86_64.artifacts.metal.formats.pxe)
@@ -35,8 +39,6 @@ kernel=$(jq -r .kernel.location    <<< "$resp")
 rootfs=$(jq -r .rootfs.location    <<< "$resp")
 initrd=$(jq -r .initramfs.location <<< "$resp")
 
-echo "content-type: application/json"
-echo
 cat <<HERE
 {
   "kernel": "$kernel",
