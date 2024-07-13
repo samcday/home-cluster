3e1fda762820d8a2a768d915b95def48688dcf7b
Sam Day <me@samcday.com>
2024-07-13 19:18:27 +0200

    cloud: external-dns

--- tofu/cloudflare.tf
+++ tofu/cloudflare.tf
@@ -6,6 +6,7 @@ resource "cloudflare_api_token" "cloud-cluster" {
   policy {
     permission_groups = [
       data.cloudflare_api_token_permission_groups.all.account["Argo Tunnel Write"],
+      data.cloudflare_api_token_permission_groups.all.account["DNS Write"],
     ]
     resources = {
       "com.cloudflare.api.account.*" = "*"
