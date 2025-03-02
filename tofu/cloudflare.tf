4ee284cbb7a96d68bc86523c22dc98fd23d5ae7a
Sam Day <me@samcday.com>
2025-03-02 19:35:53 +0100

    tf: upgrade cloudflare resources
    Signed-off-by: Sam Day <me@samcday.com>

--- tofu/cloudflare.tf
+++ tofu/cloudflare.tf
@@ -30,7 +30,7 @@ resource "kubernetes_secret" "cloudflared-tunnel-token" {
   }
 }
 
-resource "cloudflare_record" "smtp2go_cname" {
+resource "cloudflare_dns_record" "smtp2go_cname" {
   zone_id = data.cloudflare_zone.samcday.zone_id
   name    = "em510336"
   value   = "return.smtp2go.net"
@@ -38,7 +38,7 @@ resource "cloudflare_record" "smtp2go_cname" {
   proxied = false
 }
 
-resource "cloudflare_record" "smtp2go_dkim" {
+resource "cloudflare_dns_record" "smtp2go_dkim" {
   zone_id = data.cloudflare_zone.samcday.zone_id
   name    = "s510336._domainkey"
   value   = "dkim.smtp2go.net"
@@ -46,7 +46,7 @@ resource "cloudflare_record" "smtp2go_dkim" {
   proxied = false
 }
 
-resource "cloudflare_record" "smtp2go_link" {
+resource "cloudflare_dns_record" "smtp2go_link" {
   zone_id = data.cloudflare_zone.samcday.zone_id
   name    = "smtp2go-link"
   value   = "track.smtp2go.net"
